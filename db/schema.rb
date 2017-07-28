4a4875a9061578ef11b68d0ed5eb119f5b7323c1
stein328i <stein.328i@gmail.com>
2017-07-28 14:01:47 -0400

    credit_working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170727212317) do
+ActiveRecord::Schema.define(version: 20170728175910) do
 
   create_table "reservations", force: :cascade do |t|
     t.string   "name"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20170727212317) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "credits"
   end
 
 end
