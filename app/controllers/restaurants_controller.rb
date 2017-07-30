a78857c767df3a9925e6a5aa5bd8c4a74adf7553
Gina Son <ginasunah33@gmail.com>
2017-07-30 16:47:56 -0400

    Validation for reservation

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,12 +2,13 @@ class RestaurantsController < ApplicationController
 
   def index
     @restaurants = Restaurant.all
+
   end
 
   def show
     @restaurant = Restaurant.find(params[:id])
     @reservation = Reservation.new
-    # @user = User.new
+    # @reservation.date = Reservation.reservation_date
 
   end
 
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,9 +1,13 @@
 class Reservation < ApplicationRecord
-  validates :party_size, numericality: {only_integer: true}
-  validates :date, presence: true
-  validates :time, presence: true
+  validates :party_size, numericality: {less_than_or_equal_to: 30}
+  # validates :reservation_date, presence: true
+  validates :time, numericality: {greater_than_or_equal_to: 11, less_than_or_equal_to: 20}
 
   belongs_to :restaurant
   belongs_to :user
+  # 
+  # def self.reservation_date
+  #   errors.add(:reservation_date, "can't be in the past ") if !reservation_date.black? and reservation_date < Date.today
+  # end
 
 end
